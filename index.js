24fd0c83c5b08d04af3880e8e815dce8ee94d944
У М <umalat.magma@gmail.com>
2020-10-25 19:30:17 +0300

    installed hbs and bootstrap

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 //created by Michigishev Umalat
 const express = require("express")
-const app = express()
 const bodyParser = require("body-parser")
 const { Student, Group } = require('./sequelize')
+const exphbs = require('express-handlebars')
+
+const app = express()
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
 
 app.use(bodyParser.json())
+
+
 
 app.get("/", (req, res) => {
     res.status(200).send("Launched")
