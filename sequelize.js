f337ef65f3ac647a889f72c4b909b9c3c35a189d
У М <umalat.magma@gmail.com>
2020-11-03 17:27:15 +0300

    t

--- sequelize.js
+++ sequelize.js
@@ -4,8 +4,7 @@ const GroupModel = require("./models/group")
 
 const sequelize = new Sequelize('dc6ktj1usj244s', 'qbdzvigiaicjpc', '98edc681c173576a8c40f5b0c508e3298a3f441f7ebcf9b9b4e749f9bd390305', {
     host: 'ec2-54-75-199-252.eu-west-1.compute.amazonaws.com',
-    port: '5432',
-    dialect: 'postgres'
+    port: '5432'
   })
   
 const Student = StudentModel(sequelize, Sequelize)
