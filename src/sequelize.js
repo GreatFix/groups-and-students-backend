419f33fd63b87b7434bb62cfed751c52f8efda8e
У М <umalat.magma@gmail.com>
2021-01-03 20:58:09 +0300

    edit connect data

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,23 +1,31 @@
-const Sequelize = require('sequelize')
-const StudentModel = require("./models/student")
-const GroupModel = require("./models/group")
+const Sequelize = require("sequelize");
+const StudentModel = require("./models/student");
+const GroupModel = require("./models/group");
 
-const sequelize = new Sequelize('dbstudents', 'me', 'password', {
-    host: 'localhost',
-    dialect: 'postgres'
-  })
-  
-const Student = StudentModel(sequelize, Sequelize)
-const Group = GroupModel(sequelize, Sequelize)
+const sequelize = new Sequelize(
+  "dc6ktj1usj244s",
+  "qbdzvigiaicjpc",
+  "98edc681c173576a8c40f5b0c508e3298a3f441f7ebcf9b9b4e749f9bd390305",
+  {
+    host: "ec2-54-75-199-252.eu-west-1.compute.amazonaws.com",
+    port: "5432",
+    dialect: "postgres",
+  }
+);
 
-Group.hasMany(Student, { onDelete: "cascade" })
+const Student = StudentModel(sequelize, Sequelize);
+const Group = GroupModel(sequelize, Sequelize);
 
-sequelize.authenticate()
-    .then(() => console.log('Successful connection.'))
-    .catch(err => console.error('Connection error:', err)) 
+Group.hasMany(Student, { onDelete: "cascade" });
 
-sequelize.sync()
-    .then(() => console.log("Synchronized") )
-    .catch(err => console.error("Synchronization error:" + err))
+sequelize
+  .authenticate()
+  .then(() => console.log("Successful connection."))
+  .catch((err) => console.error("Connection error:", err));
 
-module.exports = { Student, Group }
+sequelize
+  .sync()
+  .then(() => console.log("Synchronized"))
+  .catch((err) => console.error("Synchronization error:" + err));
+
+module.exports = { Student, Group };
