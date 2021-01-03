33247b3dcfec7cb3b58c24c7d0003e7d02e9f805
У М <umalat.magma@gmail.com>
2021-01-03 21:00:35 +0300

    add heroku config

--- src/index.js
+++ src/index.js
@@ -1,152 +1,159 @@
 //created by Michigishev Umalat
-const express = require("express")
-const app = express()
-const bodyParser = require("body-parser")
-const { Student, Group } = require('./sequelize')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const { Student, Group } = require("./sequelize");
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.status(200).send("Launched")
-})
+  res.status(200).send("Launched");
+});
 
 app.get("/groups", async (req, res) => {
-    try {
-        const groups = await Group.findAll({ include: Student })
-        res.send(groups)
-    } catch (err){
-        res.status(400).send(err)
-    }
-})
+  try {
+    const groups = await Group.findAll({ include: Student });
+    res.send(groups);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 app.get("/groups/:id", async (req, res) => {
-    try {
-        const {id} = req.params
-        const group = await Group.findByPk(id,{ include: Student })
-        group ? res.status(200).send(group):res.sendStatus(404)
-    } catch (err){
-        res.status(400).send(err)
-    }
-})
+  try {
+    const { id } = req.params;
+    const group = await Group.findByPk(id, { include: Student });
+    group ? res.status(200).send(group) : res.sendStatus(404);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 app.post("/groups", async (req, res) => {
-    try {
-        const {name} = req.body
-        const result = await Group.create({ name: name })
-        res.status(201).send(result)
-    } catch (err){
-        res.status(400).send(err)
-    }
-})
+  try {
+    const { name } = req.body;
+    const result = await Group.create({ name: name });
+    res.status(201).send(result);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
-app.delete("/groups/:id", async (req,res) => {
-    try {
-        const {id} = req.params
-        const result = await Group.destroy({ where: { id: id }})
-        result ? res.sendStatus(200):res.sendStatus(404)
-    } catch (err){
-        res.status(400).send(err)
-    }
-})
+app.delete("/groups/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Group.destroy({ where: { id: id } });
+    result ? res.sendStatus(200) : res.sendStatus(404);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 app.get("/students", async (req, res) => {
-    try {
-        const students = await Student.findAll()
-        res.send(students)
-    } catch (err){
-        res.status(400).send(err)
-    }
-})
+  try {
+    const students = await Student.findAll();
+    res.send(students);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 app.get("/students/:id", async (req, res) => {
-    try {
-        const {id} = req.params
-        const student = await Student.findOne({where: { id: id}})
-        student ? res.status(200).send(student):res.sendStatus(404)
-    } catch (err){
-        res.status(400).send(err)
-    }
-})
+  try {
+    const { id } = req.params;
+    const student = await Student.findOne({ where: { id: id } });
+    student ? res.status(200).send(student) : res.sendStatus(404);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
 app.post("/students", async (req, res) => {
-    try {
-        const {firstName, lastName, groupId} = req.body
-        const group = await Group.findByPk(groupId)
-        if (group){
-            await group.createStudent({firstName: firstName, lastName: lastName})
-            res.sendStatus(201)
-        }else
-            res.sendStatus(404)
-    } catch (err){
-        res.status(400).send(err)
-    }    
-})
+  try {
+    const { firstName, lastName, groupId } = req.body;
+    const group = await Group.findByPk(groupId);
+    if (group) {
+      await group.createStudent({ firstName: firstName, lastName: lastName });
+      res.sendStatus(201);
+    } else res.sendStatus(404);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
-app.put("/students/:id", async(req,res) =>{
-    try {
-        const {id} = req.params
-        const {firstName,lastName,groupId} = req.body
-        if(await Student.findByPk(id)) {
-            if(firstName && lastName && groupId){
-                if(!await Group.findByPk(groupId)) res.status(404).send("Group not found") 
-                await Student.update(
-                    { 
-                    firstName: firstName,
-                    lastName: lastName,
-                    groupId: groupId
-                    },  {
-                        where: { id: id }
-                        }
-                )
-                res.status(200).send("Updated!")
+app.put("/students/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { firstName, lastName, groupId } = req.body;
+    if (await Student.findByPk(id)) {
+      if (firstName && lastName && groupId) {
+        if (!(await Group.findByPk(groupId)))
+          res.status(404).send("Group not found");
+        await Student.update(
+          {
+            firstName: firstName,
+            lastName: lastName,
+            groupId: groupId,
+          },
+          {
+            where: { id: id },
+          }
+        );
+        res.status(200).send("Updated!");
+      } else {
+        if (firstName) {
+          await Student.update(
+            {
+              firstName: firstName,
+            },
+            {
+              where: { id: id },
             }
-                else{
-                    if(firstName){
-                        await Student.update(
-                            { 
-                            firstName: firstName
-                            },  {
-                                where: { id: id }
-                                }
-                        )
-                    }
-                    if(lastName){
-                        await Student.update(
-                            { 
-                            lastName: lastName
-                            },  {
-                                where: { id: id }
-                                }
-                        )
-                    }
-                    if(groupId){
-                        if(!await Group.findByPk(groupId)) res.status(404).send("Group not found") 
-                        await Student.update(
-                            { 
-                            groupId: groupId
-                            },  {
-                                where: { id: id }
-                                }
-                        )
-                    }
-                    res.status(200).send("Updated!")
-                }
-            } else {
-                res.sendStatus(404)
+          );
+        }
+        if (lastName) {
+          await Student.update(
+            {
+              lastName: lastName,
+            },
+            {
+              where: { id: id },
             }
-    } catch (err){
-        res.status(400).send(err)
+          );
+        }
+        if (groupId) {
+          if (!(await Group.findByPk(groupId)))
+            res.status(404).send("Group not found");
+          await Student.update(
+            {
+              groupId: groupId,
+            },
+            {
+              where: { id: id },
+            }
+          );
+        }
+        res.status(200).send("Updated!");
+      }
+    } else {
+      res.sendStatus(404);
     }
-})
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
-app.delete("/students/:id", async (req,res) => {
-    try {
-        const {id} = req.params
-        const result = await Student.destroy({ where: { id: id }})
-        result ? res.sendStatus(200):res.sendStatus(404)
-    } catch (err){
-        res.status(400).send(err)
-    }
-})
+app.delete("/students/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Student.destroy({ where: { id: id } });
+    result ? res.sendStatus(200) : res.sendStatus(404);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
 
-app.listen(8080, () => console.log("Loading..."))
+let port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log("Listening on " + port);
+});
