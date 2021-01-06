e4c55fbd0f33f8480261aab406fbeb7c7b6816da
У М <umalat.magma@gmail.com>
2021-01-06 20:38:07 +0300

    fix

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post("/groups", async (req, res) => {
   }
 });
 
-app.put("/students/:id", async (req, res) => {
+app.put("/groups/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const { name } = req.body;
